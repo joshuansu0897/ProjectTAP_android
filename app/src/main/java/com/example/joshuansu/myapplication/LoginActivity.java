a7a0b629b731bf7a436dd4dafad55b7be4b3a3de
Joshua Saucedo <joshuansu0897@gmail.com>
2018-12-01 13:10:04 -0700

    change login activity

--- app/src/main/java/com/example/joshuansu/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/joshuansu/myapplication/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.joshuansu.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+
+    public void goHome(View view){
+
     }
 }
